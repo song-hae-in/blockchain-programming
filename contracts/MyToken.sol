9f2d3ea1062e9d5fa21b53a4ff2b049921ce0662
song-hae-in <1234thdgodls@naver.com>
2025-10-13 12:17:21 +0900

    add -> totalSupply, balanceOf

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -8,10 +8,28 @@ contract MyToken {
     string public symbol;
     uint8 public decimals; //
 
-    // memory .. 길이를 모르니 memory 를 이용해라,
+    uint256 public totalSupply; //
+    mapping(address => uint256) public balanceOf; // who
+
+    // contract NAME 으로 호출되는 익명함수.
     constructor(string memory _name, string memory _symbol, uint8 _decimals) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
     }
+
+    // ...
+    // public | external | ...
+
+    //     function totalSupply() external view returns (uint256) {
+    //         return totalSupply;
+    //     }
+
+    //     function balanceOf(address owner) external view returns (uint256){
+    //         return balanceOf[owner];
+    //     }
+    //     function name() external view returns (string memory) {
+    //         return name;
+    //     }
+    //
 }
