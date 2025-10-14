64c1c5a215033b6eaf529060480394ee115010cb
song-hae-in <1234thdgodls@naver.com>
2025-10-14 17:43:56 +0900

    add approve

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,12 +5,14 @@ pragma solidity ^0.8.28;
 
 contract MyToken {
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed spender, uint256 indexed amount);
     string public name;
     string public symbol;
     uint8 public decimals; //
 
     uint256 public totalSupply; //
     mapping(address => uint256) public balanceOf; // who
+    mapping(address => mapping(address => uint256)) allowance; // who, much, amount
 
     //balance check는 tx 없이 할 수 있음 모든 노드에 대해서.
 
@@ -44,6 +46,21 @@ contract MyToken {
     //         return name;
     //     }
     // ...
+    function approve(address spender, uint256 amount) external {
+        allowance[msg.sender][spender] = amount; // amount 만큼 허용.
+        emit Approval(spender, amount);
+    }
+
+    function TransferFrom(address from, address to, uint256 amount) external {
+        address spender = msg.sender;
+
+        require(allowance[from][spender] >= amount, "insufficient allowance");
+        // [owner][spender]
+        allowance[from][spender] -= amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+    }
 
     /////////////////////////////////////
     // token을 발행할때.. mint..
